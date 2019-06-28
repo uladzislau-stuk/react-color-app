a2160f56caf4c2b0bcfda4237966c40a63bf9477
Uladzislau Stuk <uladzislau.stuk@leverx.com>
2019-06-28 15:08:41 +0300

    Add animated overlay

--- src/components/pages/ColorPalette/ColorBox/ColorBox.js
+++ src/components/pages/ColorPalette/ColorBox/ColorBox.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ColorBox.scss'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const toUpperCaseFirst = (word) => word[0].toUpperCase() + word.slice(1)
+class ColorBox extends Component {
+	constructor(props) {
+		super(props);
 
-const ColorBox = ({ name, color, copyToClipboard: CopyToClipboard }) => {
-	return (
-		<CopyToClipboard text={color}>
-			<div style={{ background: color }} className="ColorBox">
-				<div className="copy">
-					<div className="copy-notification">Copied!!!</div>
-					<div className="copy-content">{color}</div>
-					<button className="copy-btn">Copy</button>
+		this.state = {
+			copied: false
+		};
+
+		this.toUpperCaseFirst = (word) => word[0].toUpperCase() + word.slice(1);
+	}
+	copy = () => {
+		this.setState({ copied: true }, () => {
+			setTimeout(() => this.setState({ copied: false }), 2000)
+		});
+	};
+
+	render() {
+		const { name, color: background } = this.props;
+		const { copied } = this.state;
+
+		return (
+			<CopyToClipboard text={background} onCopy={this.copy}>
+				<div style={{background}} className="ColorBox">
+					<div style={{background}} className={`copy-overlay${copied ? " show": ""}`} />
+					<div className={`copy-msg${copied ? " show" : ""}`}>
+						<div>Copied!!!</div>
+						<div>{background}</div>
+					</div>
+					<div className="copy">
+						<button className="copy-btn">Copy</button>
+					</div>
+					<span className="see-more">More</span>
+					<span className="color-name">{this.toUpperCaseFirst(name)}</span>
 				</div>
-				<span className="see-more">More</span>
-				<span className="color-name">{toUpperCaseFirst(name)}</span>
-			</div>
-		</CopyToClipboard>
-	);
-};
+			</CopyToClipboard>
+		);
+	}
+}
 
 ColorBox.propTypes = {
 	name: PropTypes.string.isRequired,
--- src/components/pages/ColorPalette/Palette/Palette.js
+++ src/components/pages/ColorPalette/Palette/Palette.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import uuid from 'uuid/v4'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './Palette.scss'
 
 import { ColorBox } from '../'
 
 class Palette extends Component {
 	render() {
-		const { colors } = this.props
+		const { colors } = this.props;
 
 		return (
 			<div className="Palette">
 				{colors.map((color) => (
 					<ColorBox
 						key={uuid()}
-						copyToClipboard={CopyToClipboard}
 						{...color}
 					/>
 				))}

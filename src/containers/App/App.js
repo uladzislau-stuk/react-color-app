59e970546242ee5f8c2fbc7691d98df0fcf0ffcc
Uladzislau Stuk <uladzislau.stuk@leverx.com>
2019-06-29 12:25:21 +0300

    Add generating palette

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,21 +3,25 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import configureStore from '../../redux/store/configureStore.js';
 
-import '../../utils/styles/global.scss'
+import generatePalette from '../../helpers/colorHelper.js'
+
+import { ColorPalette } from '../'
 
 import palettes from '../../test/mockData.js'
 
-import { ColorPalette } from '../'
+import '../../utils/styles/global.scss'
 
 const store = configureStore();
 
 function App() {
+  const palette = generatePalette(palettes[0]);
+
   return (
       <Provider store={store}>
           <Router>
               <Switch>
                   <Route exact path="/" render={() => <div className="alert">Hello</div>} />
-                  <Route exact path="/color-pallet/" render={() => <ColorPalette {...palettes[0]} />} />
+                  <Route exact path="/color-pallet/" render={() => <ColorPalette {...palette} />} />
               </Switch>
           </Router>
       </Provider>
